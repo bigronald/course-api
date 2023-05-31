3de84161bdf97cc6f94e2534aeef278a386a5be9
Tinotenda Muchena <whyron32@gmail.com>
2023-05-31 15:49:32 +0200

    Added requests(delete, put)

--- src/main/java/org/creatonworks/topic/TopicController.java
+++ src/main/java/org/creatonworks/topic/TopicController.java
@@ -25,9 +25,17 @@ import java.util.List;
     @RequestMapping(method = RequestMethod.POST, value = "topics")
     public void addTopic(@RequestBody Topic topic){
         topicService.addTopic(topic);
+        }
+        @RequestMapping(method = RequestMethod.PUT, value = "topics/{id}")
+        public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+            topicService.updateTopic(id, topic);    }
 
 
-}
+
+        @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+        public void deleteTopic(@PathVariable String id){
+            topicService.deleteTopic(id);
+        }
 
 }
 
